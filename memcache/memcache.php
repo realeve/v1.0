7b23bf03ea12f7df41b526522a9f18abe472be71
realeve@qq.com <lb159753>
2016-10-16 23:31:20 +0800

    删除无用文件

--- memcache/memcache.php
+++
@@ -1,7 +0,0 @@
-<?php
-$memcache = new Memcache;
-$memcache->connect('127.0.0.1',11211);
-$memcache->set('key','hello memcache!');
-$out = $memcache->get('key');
-echo $out;
-?>
--- websocket/test.bat
+++
@@ -1,3 +0,0 @@
-"E:\wamp\bin\php\php5.5.12\php.exe"  "E:\wamp\www\websocket\websocket.php"
-pause
-
