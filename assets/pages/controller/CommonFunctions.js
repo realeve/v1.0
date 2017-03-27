454f3fc38f6d93fab6153556d6bc2a3ef7df9e10
realeve <realeve@qq.com>
2017-03-27 16:08:41 +0800

    https与http自动适配

--- assets/pages/controller/CommonFunctions.js
+++ assets/pages/controller/CommonFunctions.js
@@ -636,7 +636,7 @@
     if (url == 'qualitytable' || url == 'qualitychart') {
       str = hrefTag.split('&')[0];
     } else {
-      var strTemp = window.location.href.split('?')[0].split('http://')[1].split(url)[1];
+      var strTemp = window.location.href.split('?')[0].split(window.location.protocol + '//')[1].split(url)[1];
       str = url + ((typeof strTemp == 'undefined') ? '' : strTemp);
     }
 
@@ -659,7 +659,6 @@
       $('.page-title [name="TableTitle"]').text(titleArr[titleArr.length - 1]);
     }
   }
-
 
   //系统新功能提示
   function appVersionTips() {
@@ -790,7 +789,7 @@
         avatarUrl = getRootPath(1) + '/demo/avatar/' + avatarName + '.jpg';
       }
 
-      if (avatarUrl.indexOf('http://') != -1) {
+      if (avatarUrl.indexOf(window.location.protocol + '//') != -1) {
         $.ajax({
             url: getRootPath(1) + '/datainterface/base64?src=' + avatarUrl,
           })
@@ -817,7 +816,6 @@
   function loadMenuSettings() {
     $('.page-sidebar-menu:eq(0)').html(GBK2UTF(localStorage.settings_default_menu));
   }
-
 
   function handleTable(url, obj) {
     $.ajax({
@@ -1081,8 +1079,6 @@
 
   };
 
-
-
   //侧边栏查询数据
   $('#QueryData').on('click', function() {
     $('body').toggleClass('page-quick-sidebar-open');
@@ -1223,7 +1219,6 @@
     return obj === void 0;
   }
 
-
   var tpl = (function() {
 
     var renderObj = {};
@@ -1349,7 +1344,6 @@
               data: [] //纵向排列 data.data[0].slice(1, 15)
             };
 
-
             if (typeof item.repeat != 'undefined' && item.repeat.switch) {
 
               qua.header = data.header.slice(item.repeat.start, item.repeat.end).concat(qua.header);
@@ -1393,14 +1387,11 @@
 
   })();
 
-
-
   function getWeekdayByDate(strDate) {
     var weekDay = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
     var myDate = new Date(Date.parse(strDate.replace(/-/g, "/")));
     return weekDay[myDate.getDay()];
   }
-
 
   function getSimpleEChart(obj, type) {
     type = type | 0;
@@ -1507,7 +1498,6 @@
       });
   }
 
-
   var RGB2Lab = function(RGB) {
 
     var gamma = function(x) {
@@ -1569,7 +1559,6 @@
     var X = 95.047 * var_X; //ref_X =  95.047     Observer= 2°, Illuminant= D65
     //var Y = 100 * var_Y; //ref_Y = 100.000
     var Z = 108.883 * var_Z; //ref_Z = 108.883
-
 
     //XYZ2RGB
     var_X = X / 100; //X from 0 to  95.047      (Observer = 2°, Illuminant = D65)
